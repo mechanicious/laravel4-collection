e6560bc2f679b1a039e34a8661a16a35b613e9e9
Mechanic <zuires@gmail.com>
2014-08-24 13:43:22 +0200

    api

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","ArrayAccess"],["c","ArrayIterator"],["c","CachingIterator"],["c","Closure"],["c","Countable"],["c","Illuminate\\Support\\Collection"],["c","Illuminate\\Support\\Contracts\\ArrayableInterface"],["c","Illuminate\\Support\\Contracts\\JsonableInterface"],["c","Illuminate\\Test\\InstantiationTest"],["c","Iterator"],["c","IteratorAggregate"],["c","IteratorIterator"],["c","JsonSerializable"],["c","OuterIterator"],["c","SeekableIterator"],["c","Serializable"],["c","Traversable"]];
